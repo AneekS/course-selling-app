bf367c3c3eccca7fab257a9564df2dcf318d2995
AneekS <aneeksaha2020@gmail.com>
2025-04-28 21:37:23 +0530

    added skeleton routes

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const app = express();
+
+app.post("/user/signin", function(req,res){
+    
+})
+app.post("/user/signup", function(req,res){
+
+})
+// which courses the user is opting to buy 
+app.post("/course/purchase", function(req,res){
+
+})
+//displays the purchased courses of user
+app.get("/user/purchases", function(req,res){
+
+})
+// gets all available courses on store
+app.get("purchases", function(req,res){
+
+})
+
+
+
+
+
+
+
+
+app.listen(3000);
